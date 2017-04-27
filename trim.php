9fc28d998d0b3ad9430f1e62cdfc9db5803b32fc
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-26 21:36:05 -0300

    str_word_count e trim

---
+++ trim.php
@@ -0,0 +1,10 @@
+<?php
+//trim() elimina os espaços em branco no começo e final
+//ltrim() elimina os espaçoes em branco no começo
+//rtrim() elimina os espaços em branco no final
+$txt= "   johnatan tavares cavalcante  ";
+$novo = trim($txt);
+echo $novo;
+
+
+?>
