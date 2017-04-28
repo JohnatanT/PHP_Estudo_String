64e2b2329eb45740af21dfb7ee440cc2b899e7e2
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 09:10:52 -0300

    funcao ucwords

---
+++ ucWord.php
@@ -0,0 +1,7 @@
+<?php
+
+//Deixa a primeira letra de cada palavra em maiusculo e as outras ficam como foi digitadas
+
+
+$nome = "francisco johnatan tavares cavalcante";
+echo "Meu nome é: ".ucwords($nome);
