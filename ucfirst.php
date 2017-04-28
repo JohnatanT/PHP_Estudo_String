f18cc663a3844ebdabfcdd8a2c9f6269e1b9456c
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 09:04:11 -0300

    funcao ucfirst

---
+++ ucfirst.php
@@ -0,0 +1,7 @@
+<?php
+
+//Coloca a primeira letra em maiusculo, e deixa todas as outras letras como estão
+
+$nome = "francisco Johnatan Tavares Cavalcante";
+$nome2 = ucfirst($nome);
+echo "Meu nome é: $nome2";
