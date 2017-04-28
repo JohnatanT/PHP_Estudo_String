ca8775f9d7cd29d4c8bd684a0593e2bf1f79a412
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 09:54:40 -0300

    funcao substr

---
+++ substr.php
@@ -0,0 +1,9 @@
+<?php
+
+//vai receber uma substring de uma string e corta-la
+$frase = "Curso em video";
+$sub = substr($frase,0,5); // substr(variavel,posição inicial,até onde vai pegar)
+//pegou a variavel $frase, pegando a posição inicial 0 e vai andar 5 letras.
+echo $sub;
+
+
