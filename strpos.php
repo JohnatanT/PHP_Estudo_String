e852895402a53f9575bd9441ea5fe414dcda8061
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 09:41:28 -0300

    funcao stripos

--- strpos.php
+++ strpos.php
@@ -1,7 +1,16 @@
 <?php
 
-//Vai verificar qual posição uma substring se encontra dentro de uma string
+//Vai verificar qual posição uma substring se encontra dentro de uma string(A palavra buscada tem que ser escrita como está na frase)
 
 $frase = "Aprendendo PHP";
 $pos = strpos($frase,"PHP");
-echo "A frase : <br/> $frase</br> Esta na posição: $pos";
+echo "A frase : <br/> $frase</br> Esta na posição: $pos <br/>";
+
+
+
+//Funcao stripos tem a mesma função da strpos, mas ela busca a plavra independente se é masiucula ou minuscula(ignoreCase) 
+
+$fras = "Aprendendo PHP";
+$posi = stripos($fras,"php");
+
+echo "A posição foi: $posi";
