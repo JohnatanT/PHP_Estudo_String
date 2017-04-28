6c12f3f7616a0d1c11d60308a58671e7775ac833
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 09:07:18 -0300

    Juncao da funcao strtolower com a ucfirst

--- ucfirst.php
+++ ucfirst.php
@@ -5,3 +5,10 @@
 $nome = "francisco Johnatan Tavares Cavalcante";
 $nome2 = ucfirst($nome);
 echo "Meu nome é: $nome2";
+
+
+//POdemos fazer assim também
+$nome3 = "francisco Johnatan Tavares Cavalcante";
+/* Estou deixando tudo em minusculo comn o strtolower e depois deixando a primeira letra em maiusculo*/
+$nome4 = ucfirst(strtolower($nome3));
+echo "Meu nome é: $nome4";
