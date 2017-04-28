073669deca363cb22cb068144fae8aae1d6f5d40
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 09:36:04 -0300

    funcao strpos

---
+++ strpos.php
@@ -0,0 +1,7 @@
+<?php
+
+//Vai verificar qual posição uma substring se encontra dentro de uma string
+
+$frase = "Aprendendo PHP";
+$pos = strpos($frase,"PHP");
+echo "A frase : <br/> $frase</br> Esta na posição: $pos";
