7ca6fc7bce545f2076e2906b1f64d459e5784b4d
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-28 10:01:00 -0300

    funcao str_pad

--- str_pad.php
+++ str_pad.php
@@ -1,8 +1,9 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//vai fazer uma String caber em determinado espaço
 
+$nome = "Johnatan";
+$novo = str_pad($nome,30," ",STR_PAD_RIGHT);
+//str_pad(variavel,espaço a ser preenchido, o que colocar para preencher, para onde deve preencher);
+
+//No exemplo: str_pad(Pega variavel $nome, usa-la em 30 espaços, preencher com espaços, preencher a direita);
